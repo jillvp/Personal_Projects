0964e4026d0fd105bf4e7ffc966e71824a77ee02
Jill van Put <52222203+jillvp@users.noreply.github.com>
2019-10-21 17:24:49 +0200

    madlibs.py

---
+++ Star Wars Mad Libs Game/madlibs.py
@@ -0,0 +1,24 @@
+# A GALAXY APART: STAR WARS YOUR WAY AD-LIB
+
+print("<<<<<<<<<<<<<<<<<<<<<<<<< Mad Libs - A Galaxy Apart >>>>>>>>>>>>>>>>>>>>>>>>>")
+
+name = input("Tell me your name, and click enter. ")                            
+adj1 = input("Tell me an adjective, and click enter. ")                             
+verb1 = input("Tell me a verb, and click enter. ")                              
+sillyword = input("Tell me a silly word, and click enter. ")                    
+noun1 = input("Tell me a noun, and click enter. ")                             
+noun2 = input("Tell me another noun (plural), and click enter. ")              
+verb2 = input("Tell me a verb (plural, ending in 'ed'), and click enter. ")    
+noun3 = input("Tell me a noun, and click enter. ")                             
+noun4 = input("Tell me a noun, and click enter. ")                             
+verb3 = input("Tell me a verb (plural, ending in 'ed'), and click enter. ")    
+adj2 = input("Tell me an adjective, and click enter. ")                        
+      
+print('\n\nDarth {0} looked at his master while his {1} cold breathing filled the room.\nHe was told to go to {2} everything on the planet of {3}.\nHe got in his {4} and to hyperspace.\nSoon before he reached the planet, he dropped out of hyperspace and was attacked by Rebel {5}'.format(name, adj1, verb1, sillyword, noun1, noun2))
+print('He {0} them off and continued to the planet`s surface.\nHe landed and confronted more opposition, slicing it down with his {1}.\nHe used the {2} to choke another Rebel, then {3} him aside.\nHe finished off all life on the planet with a/an {4} laugh.'.format(verb2, noun3, noun4, verb3, adj2))
+      
+      
+
+
+
+
